1147e320bc54d623dd805debb491115cdeb72374
Brian Hogan / New Auburn Personal Computer Services <bphogan@gmail.com>
2015-08-15 13:29:23 -0500

    Comment out a section that should be a comment.

--- 06_conversion.exs
+++ 06_conversion.exs
@@ -1,7 +1,8 @@
 Float.parse("20")
 Float.parse("20abc")
 Float.parse("abc")
-Tuples are the return values of lots of functions in Elixir. And using pattern matching, we can extract what we need.
+
+# Tuples are the return values of lots of functions in Elixir. And using pattern matching, we can extract what we need.
 
 {number, _junk} = Float.parse("20abc")
 number
